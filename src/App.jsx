68133d1bd34df5ade85fd8afa2a6f20c7b989be7
Sanjeev Kumar <sanjeevkumar201418@gmail.com>
2020-10-27 14:40:05 +0530

    add dog widget

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import DogWidget from "./DogWidget"
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => <div><DogWidget/></div>;
 
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/DogWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+    <img src="https://placedog.net/500/280?id=5" style={{
+        height: 280, width: 500,
+        padding: "1em",
+        border: "1em dashed red"
+    }} />
+)
